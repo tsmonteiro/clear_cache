fe699f1e35489f6d6822cb7fd16e632d6b5ed7bf
Thiago Monteiro <tsmonteiro@gmail.com>
2022-05-12 16:08:05 +0000

    Support for multiple documentId columns

--- main.R
+++ main.R
@@ -4,38 +4,48 @@ library(tim)
 
 ctx = tercenCtx()
 
-if (!any(ctx$cnames == "documentId")) stop("Column factor documentId is required") 
 
-df <- ctx$cselect()
-docId <- df$documentId
+colNames <- ctx$cnames
+# grepl( needle, haystack, fixed = TRUE)
+# Checking for documentId columns
+docIdCols <- unname(unlist(colNames[unlist(lapply(colNames, function(x){
+  return(grepl("documentId", x, fixed = TRUE))
+} ))]))
 
-f.names <- tim::load_data(ctx, docId, force_load=FALSE)
-
-
-if( length(f.names) > 0){
-  baseDir   <- dirname( f.names[1] )
-  baseDirUp <- dirname( baseDir ) # This is where the locks will be for a zip file
+if (length(docIdCols) == 0 || length(docIdCols) > 2) stop("Either 1 or 2 documentId columns expected.") 
+ctx$cselect(unique(unlist(docIds[1])))
+for( docIdCol in docIds ){
+  docId <- ctx$cselect(unique(unlist(docIdCol)))
   
-  if( file.exists(file.path(baseDir, '.downloaded') ) ){
-    unlink(file.path(baseDir, '.downloaded'))
+  
+  f.names <- tim::load_data(ctx, docId, force_load=FALSE)
+  
+  
+  if( length(f.names) > 0){
+    baseDir   <- dirname( f.names[1] )
+    baseDirUp <- dirname( baseDir ) # This is where the locks will be for a zip file
+    
+    if( file.exists(file.path(baseDir, '.downloaded') ) ){
+      unlink(file.path(baseDir, '.downloaded'))
+    }
+    
+    if( file.exists(file.path(baseDir, '.downloaded') ) ){
+      unlink(file.path(baseDir, '.downloaded'))
+    }
+    
+    if( file.exists(file.path(baseDirUp, '.downloaded') ) ){
+      unlink(file.path(baseDirUp, '.downloaded'))
+    }
+    
+    if( file.exists(file.path(baseDirUp, '.extracted') ) ){
+      unlink(file.path(baseDirUp, '.extracted'))
+    }
   }
   
-  if( file.exists(file.path(baseDir, '.downloaded') ) ){
-    unlink(file.path(baseDir, '.downloaded'))
+  for( fname in f.names ){
+    print(fname)
+    unlink(fname)
   }
-  
-  if( file.exists(file.path(baseDirUp, '.downloaded') ) ){
-    unlink(file.path(baseDirUp, '.downloaded'))
-  }
-  
-  if( file.exists(file.path(baseDirUp, '.extracted') ) ){
-    unlink(file.path(baseDirUp, '.extracted'))
-  }
-}
-
-for( fname in f.names ){
-  print(fname)
-  unlink(fname)
 }
 
 ctx$save(ctx$select())
